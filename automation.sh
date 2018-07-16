c54ae8c2c2614d6c07d4039a6b757c30342ffafd
Sunny Kumar <sunny@peacehaven.co>
2018-07-16 14:40:38 -0400

    Stelligent Project work
    Single command line execution of project
    
    Deployment - The below command launches a ec2 instance
    
    C:\Users\sunny>aws ec2 run-instances --image-id ami-6871a115  --count 1 --instance-type t2.micro --key-name sunny --security-groups default --user-data C:\Users\sunny\Desktop\Stelligent\automation.sh

---
+++ automation.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+sudo su -
+yum update -y
+yum install -y httpd php
+service httpd start
+chkconfig httpd on
+usermod -a -G www ec2-user
+chown -R root:www /var/www
+chmod 2775 /var/www
+find /var/www -type d -exec chmod 2775 {} +
+find /var/www -type f -exec chmod 0664 {} +
+cd /var/www/html
+echo "Automation for the People" > index.html
